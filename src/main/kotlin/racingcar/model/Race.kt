f00e82e51c124e374fa5ee2500d287cd6b67e3d9
박병훈 <leafloveu@naver.com>
2025-11-16 22:18:59 +0900

    feat(race): 경주 생성시, 시도횟수가 범위를 벗어나면 예외 발생

--- src/main/kotlin/racingcar/model/Race.kt
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -1,9 +1,20 @@
 package racingcar.model
 
+import racingcar.error.ErrorCode
 import racingcar.model.dto.RaceRecord
 import racingcar.model.dto.RoundRecord
 
 class Race(private val trialCount:Int, private val participants: Participants) {
+
+    init {
+        validate(trialCount)
+    }
+
+    private fun validate(trialCount: Int) {
+        if (trialCount < 1 || trialCount > 1000) {
+            throw IllegalArgumentException(ErrorCode.OUT_OF_RANCE_TRIAL_COUNT.message())
+        }
+    }
 
     fun start(): RaceRecord {
         var raceRecord = RaceRecord()
