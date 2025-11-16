12748037d16e16ae320bc5324291a3093038ea75
박병훈 <leafloveu@naver.com>
2025-11-17 00:18:03 +0900

    refactor: Util class -> object 변경

--- src/main/kotlin/racingcar/util/IntParser.kt
+++ src/main/kotlin/racingcar/util/IntParser.kt
@@ -2,15 +2,13 @@ package racingcar.util
 
 import racingcar.error.ErrorCode
 
-class IntParser {
+object IntParser {
 
-    companion object {
-        fun parse(input: String): Int {
-            try {
-                return input.toInt()
-            } catch (_: NumberFormatException) {
-                throw NumberFormatException(ErrorCode.INVALID_INT_FORMAT.message())
-            }
+    fun parse(input: String): Int {
+        try {
+            return input.toInt()
+        } catch (_: NumberFormatException) {
+            throw NumberFormatException(ErrorCode.INVALID_INT_FORMAT.message())
         }
     }
 }
