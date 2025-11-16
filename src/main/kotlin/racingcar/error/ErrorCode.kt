893e68b4507accc5acc1fa7d218bea04059bde2e
박병훈 <leafloveu@naver.com>
2025-11-16 21:57:13 +0900

    test(participants): 참가 자동차 총 개수 2~10개 만족 테스트 추가

--- src/main/kotlin/racingcar/error/ErrorCode.kt
+++ src/main/kotlin/racingcar/error/ErrorCode.kt
@@ -3,7 +3,8 @@ package racingcar.error
 enum class ErrorCode(private val message: String) {
 
     INVALID_CAR_NAME("잘못된 자동차 이름입니다. 자동차 이름은 문자와 숫자로만 이루어진 1~5자 문자열만 가능합니다."),
-    DUPLICATED_CAR_NAME("중복된 자동차 이름입니다. 경주할 자동차의 이름은 중복되면 안됩니다.")
+    DUPLICATED_CAR_NAME("중복된 자동차 이름입니다. 경주할 자동차의 이름은 중복되면 안됩니다."),
+    OUT_OF_RANGE_PARTICIPANTS_COUNT("허용된 자동차 개수를 벗어났습니다. 자동차는 최소 2대 ~ 최대 10대까지만 가능합니다."),
     ;
 
     fun message(): String {
--- src/test/kotlin/racingcar/model/ParticipantsTest.kt
+++ src/test/kotlin/racingcar/model/ParticipantsTest.kt
@@ -3,6 +3,8 @@ package racingcar.model
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.error.ErrorCode
 
 class ParticipantsTest {
@@ -33,11 +35,32 @@ class ParticipantsTest {
             .hasMessage(ErrorCode.DUPLICATED_CAR_NAME.message())
     }
 
+    @DisplayName("참가자 생성 실패 테스트 - 전체 자동차 개수가 2~10개 범위를 벗어남")
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "car1",
+            "car1,car2,car3,car4,car5,car6,car7,car8,car9,car10,car11"
+        ]
+    )
+    fun participantsCountOutOfRangeTest(names: String) {
+        // given
+        val split = names.split(",")
+        val cars = split
+            .map { name -> Car(name) { true } }
+            .toList()
+
+        // when & then
+        assertThatThrownBy { Participants(cars) }
+            .isExactlyInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(ErrorCode.OUT_OF_RANGE_PARTICIPANTS_COUNT.message())
+    }
+
     @DisplayName("참가자 전원 전진 1회 테스트")
     @Test
     fun moveAllTest() {
         // given
-        val cars = listOf("pobi", "woni", "jun", "pobi")
+        val cars = listOf("pobi", "woni", "jun")
             .map { name -> Car(name) { true } }
             .toList()
         val participants = Participants(cars)
