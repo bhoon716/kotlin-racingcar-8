d9df3e16c8c4e622d0984d5337654c04e41f616e
박병훈 <leafloveu@naver.com>
2025-11-16 21:56:06 +0900

    feat(participants): 참가자 전원 1회 전진 시도, 이름 중복 시 예외 발생 추가

--- src/main/kotlin/racingcar/model/Participants.kt
+++ src/main/kotlin/racingcar/model/Participants.kt
@@ -1,8 +1,20 @@
 package racingcar.model
 
+import racingcar.error.ErrorCode
 import racingcar.model.dto.CarRecord
 
 class Participants(private val participants: List<Car>) {
+
+    init {
+        validate(participants)
+    }
+
+    private fun validate(participants: List<Car>) {
+        val names = participants.map { it.name }
+        if (names.toSet().size != participants.size) {
+            throw IllegalArgumentException(ErrorCode.DUPLICATED_CAR_NAME.message())
+        }
+    }
 
     fun tryToMoveAll() {
         participants.forEach(Car::tryToMove)
